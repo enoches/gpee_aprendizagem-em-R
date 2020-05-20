4df5d25c05d3c5dc6750f69bf3b2d026993c88e9
Enoch Sousa Filho <enoches@gmail.com>
2020-05-20 17:59:01 -0300

    Instruções iniciais

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# estudos-metodos_grupo-de-espacial
-Grupo de estudos de métodos estatísticos/ econométricos/ machine learning (grupo de pesquisas em economia espacial da UFBA 
+ 
+# Grupo de estudos do grupo de economia espacial da UFBA =D  
+
+20-maio-2020
+
+
+
+O objetivo é estudar a linguagem R: métodos estatísticos/ econométricos/ machine learning...  
+
+Vamos replicar os códigos e exercícios do livro [An Introduction to Statistical Learning: With Applications in R](http://faculty.marshall.usc.edu/gareth-james/ISL/) (ISLR).  
+
+
+
+
+
+## Preparativos    
+
+### Para quem for baixar o programa no computador: 
+
+- Instalar o R 4.0 (https://cran.rstudio.com/bin/windows/base/)  
+- Instalar o RStudio (preview da próxima versão, mas que está estável e dá pra usar tranquilamente) (https://rstudio.com/products/rstudio/download/preview/)  
+- Instalar os programas do script `instalar-pacotes.R`
+
+
+
+### Para quem for usar diretamente na internet, sem instalar:  
+
+- Criar uma conta gratuita no RStudio Cloud (https://rstudio.cloud/)   
+- Entrar neste projeto: (https://rstudio.cloud/project/1291558)  
+- Clicar em "Save a permanent copy"
+
+
+
+## OBS 
+
+Este repositório está no github: (https://github.com/enoches/gpee_aprendizagem-em-R)
+
---
+++ instalar-pacotes.R
@@ -0,0 +1,38 @@
+# Este script lista os pacotes que vamos instalar inicialmente 
+
+# Script criado em 2020-05-20; Enoch Filho
+
+
+
+# Antes de instalar, verifique se já não possui os pacotes instalados.
+
+
+
+
+# Primeiro estes: ---------------------------------
+
+
+# O tidyverse instala um conjunto de pacotes (dplyr, tidyr, forcats, stringr, 
+# lubridate, purrr, etc) então vai demorar um pouco. 
+
+install.packages("tidyverse") # Easily Install and Load the 'Tidyverse'
+install.packages("here") # A Simpler Way to Find Your Files
+install.packages("skimr") # Compact and Flexible Summaries of Data
+install.packages("datapasta") # R Tools for Data Copy-Pasta
+install.packages("ISLR") # Data for an Introduction to Statistical Learning with Applications in R
+
+
+
+# Depois estes:  -------------------------------------
+
+
+# (deixe pra depois se tiver em cima do horário) 
+
+install.packages("tidymodels") # Easily Install and Load the 'Tidymodels' Packages
+install.packages('remotes') # R Package Installation from Remote Repositories, Including 'GitHub'
+install.packages("moderndive") # Tidyverse-Friendly Introductory Linear Regression
+install.packages("AER") # Applied Econometrics with R
+install.packages("car") # Companion to Applied Regression
+install.packages("estimatr") # Fast Estimators for Design-Based Inference
+install.packages("gapminder") # Data from Gapminder
+install.packages("sf") # Simple Features for R
