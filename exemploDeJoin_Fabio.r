bda34d5e3db58442c0759c06cb263d73c714a162
Enoch Sousa Filho <enoches@gmail.com>
2020-08-12 16:38:12 -0300

    Insere exemplo Unicornios em Monociclos

--- exemploDeJoin_Fabio.r
+++ exemploDeJoin_Fabio.r
@@ -1,8 +1,55 @@
-x1 <-  data.frame(CodMun = c(123,456,789),Pop = (1:3)*10000)
-x2  <- data.frame(CodMun = c(111,123,456,888), PIB = (5:8)*1000)
+# Exemplos de joins - por Fabio Fernandes
+# Atualizado em 05/08/2020
 
-left_join(x1,x2,by="CodMun")
-right_join(x1,x2,by="CodMun")
-full_join(x1,x2,by="CodMun")
-inner_join(x1,x2,by="CodMun")
 
+# Dados -------------------------------------------------------------------
+
+
+x1 <-  data.frame(CodMun = c(123, 456, 789), 
+                  Pop = (1:3) * 10000
+                  )
+
+x2  <- data.frame(CodMun = c(111, 123, 456, 888), 
+                  PIB = (5:8) * 1000
+                  )
+
+x3  <- data.frame(CodMun = c(111, 123, 456, 888, 123), 
+                  PIB = (5:9) * 1000
+                  )
+
+
+
+# Juntar x1 e x2 ----------------------------------------------------------
+
+library(dplyr) # para usar as funções `*_join()`
+
+left_join(x1, x2, by = "CodMun")
+right_join(x1, x2, by = "CodMun")
+
+full_join(x1, x2, by = "CodMun") 
+inner_join(x1, x2, by = "CodMun")
+
+
+
+
+# Juntar x1 e x3, sendo que x3 contém CodMun repetido ---------------------
+
+# library(dplyr)
+
+left_join(x1, x3, by = "CodMun")
+right_join(x1, x3, by = "CodMun")
+
+full_join(x1, x3, by = "CodMun") 
+inner_join(x1, x3, by = "CodMun")
+
+
+
+
+# Extra -------------------------------------------------------------------
+
+
+# Quais são todos os CodMun que existem nas duas bases?
+union(x1$CodMun, x2$CodMun) # similar ao full_join()
+
+# Quais são os codmun que existem nas duas bases ao mesmo tempo?
+intersect(x1$CodMun, x2$CodMun) # similar (mas não igual) ao inner_join() 
--- montar-base-de-dados-2020-07-29.R
+++ montar-base-de-dados-2020-07-29.R
@@ -1,5 +1,5 @@
 # Este script vai criar um banco de dis arquivos Excel diferentes. 
-# Enoch Filho, 29/07/2020
+# Enoch Filho, 29/07/2020;  Atualizado em 12/08/2020
 
 # Vamos juntos fazer um exercício de limpar e juntar os dados que estão em dois 
 # arquivos Excel diferentes. Espera-se um script que possa ler os arquivos 
@@ -76,7 +76,7 @@ isTRUE(sales$year...2 == sales$year...7)
 
 
 # Agora vamos verificar a variável para ano, que também parece duplicada:
-identical(sales$year...2, sales$year...7)
+waldo::compare(sales$year...2, sales$year...7)
 
 
 
@@ -85,14 +85,127 @@ all(is.na(sales$...4))
 all(is.na(sales$...5))
 
 
-# Agora que sabemos que está tudo ok vamos retirar as variáveis duplicadas/ em branco 
+# Agora que sabemos que está tudo ok vamos manter apenas as variáveis que interessam,
+# sem duplicidade nem variávei "em branco": 
 
+# Podemos declarar as variáveis que queremos manter:
 sales_drv <- sales %>% 
     select(name_of_country...1, year...2, bikes, total_turnover) 
-    
 
-# ATT: DEVER DE CASA: 
-# -[] Juntar sales_drv + observations 
+# Ou poderíamos declarar as variáveis que queremos retirar:
+sales %>% 
+    select( -c(...4, ...5, name_of_country...6, year...7) ) %>% # explicita os nomes 
+    colnames()
+
+sales %>% 
+    select( -c(4:7) ) %>% # tirar quarta à sétima variável do bd sales
+    colnames() 
+
+
+# ATT: DEVER DE CASA: ----
+# -[x] Juntar sales_drv + observations 
+
+sales_drv %>% colnames()
+observations %>% colnames()
+
+sales_drv %>% distinct(name_of_country...1)
+
+sales_drv %>%  
+    left_join(observations,
+        by = c( "name_of_country...1" =  "countryname", "year...2" = "year")
+    ) %>% View("left")
+
+sales_drv %>%  
+    inner_join(observations,
+        by = c( "name_of_country...1" =  "countryname", "year...2" = "year")
+    ) %>% View("inner")
+
+sales_drv %>%  
+    full_join(observations,
+        by = c( "name_of_country...1" =  "countryname", "year...2" = "year")
+    ) %>% View("full")
+
+
+inner_join(sales_drv, observations, by = c("year...2" = "year")) %>% View("ex")
+
+inner_join(sales_drv, observations, by = c("year...2" = "year")) %>% 
+    filter(name_of_country...1 == "AUSTRIA", year...2 == 1671) %>% 
+    View("ex2")
+
+
+
+# Pra dar certo temos resolver a grafia dos nomes dos países
+
+# library(stringr) 
+# str_to_title("AUSTRIA") 
+
+sales_drv <- 
+mutate(sales_drv, 
+    name_of_country...1 = str_to_title(name_of_country...1)
+    )
+
+# sales_drv$pais <- str_to_title(sales$name_of_country...1)
+# sales_drv$name_of_country...1 <- str_to_title(sales$name_of_country...1)
+
+# Agora sim! Podemos fazer o join!
+
+
+sales_drv <- sales_drv %>%  
+    left_join(observations,
+        by = c( "name_of_country...1" =  "countryname", "year...2" = "year")
+    ) 
+
+sales_tidy <- sales_drv %>% 
+    rename(pais = name_of_country...1,
+        year = year...2)
+
+
+# Dica: salvar no disco e carregar o banco de dados pronto ----------------
+
+# Salvar ou carregar o banco de dados pronto para análise:
+
+saveRDS(sales_tidy, file = here("sales_tidy.rds")) # salvar no disco
+
+sales_tidy <- readRDS(here("sales_tidy.rds")) # carregar do disco
+
+
+
+
+# ATIVIDADE PARA 12/08/2020 -----------------------------------------------
+
+
+## Exercício 01 - 12/08/2020 ----
+# Refaça o banco de dados sales_tidy, mas dessa vez faça o encadeamendo do código 
+# "em uma única linha", isto é, evitando ao maximo salvar objetos intermediários 
+# para chegar ao resultado final.   
+
+# Ex: 
+# sales_tidy <- 
+#     passo1 %>% 
+#     passo2 %>% 
+#     . 
+#     . 
+#     . 
+#     passo_n
+
+
+
+
+
+
+## Exercício 02 - 12/08/2020 ----
+# A partir do banco de dos sales_tidy, use a biblioteca tidyr, calcule o 
+# preço médio do monociclo em cada ano para cada país. Em seguida monte essas 
+# informações numa tabela assim: 
+
+# pais    1670   1671   1672   ...
+# pais1    a      b      c
+# pais2    d      e      f
+
+
+
+
+
 
 
 
